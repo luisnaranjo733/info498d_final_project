602dfb700bb3aa90e537c551771044b9eee38104
Luis Naranjo <luisnaranjo733@gmail.com>
2016-02-25 10:12:29 -0800

    added action bar with settings button, added click listener

--- app/src/main/java/info498d/uw/edu/smartalarm/MainActivity.java
+++ app/src/main/java/info498d/uw/edu/smartalarm/MainActivity.java
@@ -1,13 +1,37 @@
 package info498d.uw.edu.smartalarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    protected static String TAG = "**SmartAlarm.Main";
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        this.menu = menu;
+        MenuItem settingsItem = menu.findItem(R.id.open_settings_option);
+        settingsItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.v(TAG, "Opening settings activity");
+                // Return true to consume this click and prevent others from executing.
+                return true;
+            }
+        });
+        return true;
     }
 }
