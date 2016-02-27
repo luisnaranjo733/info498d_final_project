f59a24c08a1823772de8adc215537a50346f1184
Jooneil Ahn <ahnj12@uw.edu>
2016-02-27 15:12:20 -0800

    prefence settings created, creating issues from complex preferences

---
+++ app/src/main/java/info498d/uw/edu/smartalarm/SettingsActivity.java
@@ -0,0 +1,16 @@
+package info498d.uw.edu.smartalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+        //setContentView(R.layout.activity_settings);
+    }
+}
---
+++ app/src/main/java/info498d/uw/edu/smartalarm/SettingsFragment.java
@@ -0,0 +1,43 @@
+package info498d.uw.edu.smartalarm;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+
+        Preference sleepPreference = findPreference("pref_key_sleep_locations");
+        sleepPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(), SleepLocations.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+
+    }
+}
