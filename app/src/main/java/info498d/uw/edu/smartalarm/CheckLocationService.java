b315f767c4e6540b331576d48ff6d76e13495f4f
Jooneil Ahn <ahnj12@uw.edu>
2016-03-08 22:20:18 -0800

    added charging

--- app/src/main/java/info498d/uw/edu/smartalarm/CheckLocationService.java
+++ app/src/main/java/info498d/uw/edu/smartalarm/CheckLocationService.java
@@ -108,15 +108,12 @@ public class CheckLocationService extends Service {
             public void gotLocation(Location location) {
                 SharedPreferences sp = getSharedPreferences("GLOBAL", Context.MODE_PRIVATE);
                 Boolean charging = sp.getBoolean("charging", false);
+                Log.v(TAG, "current charging: " + charging);
                 if (charging) {
                     Log.v(TAG, "got person location");
                     Log.v(TAG, location.toString());
                     Log.v(TAG, sleepLocations);
                 }
-
-
-
-
             }
         };
         final Context context = this;
